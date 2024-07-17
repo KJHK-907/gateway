2ef39997d3a3e2d6d2fe5b3a6c1a418f5012f9f6
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-07-16 22:00:06 -0500

    feat: Add WebSocket handler for client communication

---
+++ handlers/websocketHandler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(msg))
+	}
+}

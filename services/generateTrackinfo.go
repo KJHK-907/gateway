f2e0a87fcefca78c0258b41e96a9e27b36d0491e
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-07-30 23:07:40 -0500

    feat: Generate track information from metadata

---
+++ services/generateTrackinfo.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"gateway/models"
+	"log"
+	"strconv"
+	"time"
+)
+
+func GenerateTrackinfo(currentMetadata models.ZettaLite) models.Trackinfo {
+	totalLength, err := strconv.ParseFloat(currentMetadata.LogEventCollection.LogEvent.Asset[0].TotalLength, 64)
+	if err != nil {
+		log.Printf("Error converting TotalLength to int: %v", err)
+		totalLength = 0.0
+	}
+	startTimeLocal, err := time.Parse("1/2/2006 3:04:05 PM", currentMetadata.LogEventCollection.LogEvent.StartTimeLocal)
+	if err != nil {
+		log.Printf("Error parsing StartTimeLocal: %v", err)
+		startTimeLocal = time.Now()
+	}
+	timestampCST := startTimeLocal.Format("2006-01-02T15:04:05Z")
+	trackInfo := models.Trackinfo{
+		Track:        currentMetadata.LogEventCollection.LogEvent.Asset[0].Title,
+		Album:        currentMetadata.LogEventCollection.LogEvent.Asset[0].Album1,
+		Artist:       currentMetadata.LogEventCollection.LogEvent.Asset[0].Artist1,
+		Length:       totalLength * 1000,
+		TimestampUTC: currentMetadata.LogEventCollection.LogEvent.StartTime,
+		TimestampCST: timestampCST,
+	}
+	return trackInfo
+}

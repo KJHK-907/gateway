bd195708f1ec6260129c9ba5aa44c9876180bbfb
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-07-31 16:18:45 -0500

    feat: check trackinfo map for duplicates & cleanup old entries

--- services/xmlProcessor.go
+++ services/xmlProcessor.go
@@ -1,12 +1,21 @@
 package services
 
 import (
-	"strings"
-
 	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"gateway/models"
 )
 
-func ProcessXml(data []byte, buffer *strings.Builder) {
+var (
+	recentTrackInfo = make(map[string]time.Time)
+	mu              sync.Mutex
+)
+
+func ProcessXml(data []byte, buffer *strings.Builder, pool *models.Pool) {
 	buffer.Write(data)
 
 	for strings.Contains(buffer.String(), "</ZettaLite>") {
@@ -25,14 +34,39 @@ func ProcessXml(data []byte, buffer *strings.Builder) {
 			buffer.Reset()
 			continue
 		}
-		MetadataChannel <- currentMetadata
-		println("Received metadata from Zetta RCS:")
-		fmt.Printf("%+v\n", currentMetadata)
+		trackInfo := GenerateTrackinfo(currentMetadata)
+		// Broadcast the track info to all connected clients
+
+		// Check if trackinfo is in the recentTrackInfo map
+		mu.Lock()
+		if _, ok := recentTrackInfo[trackInfo.Track]; ok {
+			mu.Unlock()
+			buffer.Reset()
+			continue
+		}
+		recentTrackInfo[trackInfo.Track] = time.Now()
+		mu.Unlock()
+
+		pool.Broadcast <- trackInfo
+		log.Println("Received metadata from Zetta RCS:")
+		fmt.Printf("%+v\n", trackInfo)
 		println("--------------------------")
 
 		// Remove the parsed document from the buffer
-		substring := buffer.String()[end:]
-		buffer = &strings.Builder{}
-		buffer.WriteString(substring)
+		buffer.Reset()
+	}
+}
+
+func CleanupOldEntries() {
+	for {
+		time.Sleep(time.Hour) // Run cleanup every hour
+
+		mu.Lock()
+		for track, timestamp := range recentTrackInfo {
+			if time.Since(timestamp) > time.Hour { // Clear entries older than 1 hour
+				delete(recentTrackInfo, track)
+			}
+		}
+		mu.Unlock()
 	}
 }

f659f7b7e92d1c664a41cdd590199bd7ba416220
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-14 21:42:04 -0500

    refactor: Remove unused code in websocketHandler.go

--- handlers/websocketHandler.go
+++ handlers/websocketHandler.go
@@ -6,17 +6,7 @@ import (
 
 	"gateway/models"
 	"gateway/services"
-
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
 
 func HandleWebSocket(pool *models.Pool, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
